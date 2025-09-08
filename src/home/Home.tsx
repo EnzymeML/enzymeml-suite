7a6f81c6d8136770e30e739a93edee72c21245bd
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-09-08 10:17:24 +0200

    Add small molecule DB listing to Home component
    Imported and called getAllSmallMolecules in the Home component to list all small molecule databases. Also refactored code formatting and updated JsonView usage for improved readability.

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,182 +1,193 @@
-import React, {useEffect, useState} from 'react';
-import {emit, listen} from '@tauri-apps/api/event'
-import {allExpanded, defaultStyles, JsonView} from 'react-json-view-lite';
-import 'react-json-view-lite/dist/index.css';
+import React, { useEffect, useState } from "react";
+import { emit, listen } from "@tauri-apps/api/event";
+import "react-json-view-lite/dist/index.css";
 import {
-    DBEntries,
-    EnzymeMLState,
-    exportToJSON,
-    getState,
-    listEntries,
-    loadEntry,
-    loadJSON,
-    newEntry,
-    saveEntry
+  DBEntries,
+  EnzymeMLState,
+  exportToJSON,
+  getState,
+  listEntries,
+  loadEntry,
+  loadJSON,
+  newEntry,
+  saveEntry,
 } from "../commands/dataio.ts";
-import {Button, Input, Select} from "antd";
-import {setTitle} from "../commands/enzmldoc.ts";
-import NotificationProvider, {NotificationType} from "../components/NotificationProvider.tsx";
+import { Button, Input, Select } from "antd";
+import { setTitle } from "../commands/enzmldoc.ts";
+import NotificationProvider, {
+  NotificationType,
+} from "../components/NotificationProvider.tsx";
 import useAppStore from "../stores/appstore.ts";
-
+import { JsonView, defaultStyles } from "react-json-view-lite";
+import { getAllSmallMolecules } from "../commands/dbops.ts";
 export default function Home() {
+  // States
+  const [currentDoc, setCurrentDoc] = useState<EnzymeMLState | null>(null);
+  const [documents, setDocuments] = useState<DBEntries[]>();
 
-    // States
-    const [currentDoc, setCurrentDoc] = useState<EnzymeMLState | null>(null);
-    const [documents, setDocuments] = useState<DBEntries[]>()
+  // Actions
+  const openNotification = useAppStore((state) => state.openNotification);
 
-    // Actions
-    const openNotification = useAppStore(state => state.openNotification);
+  // Effects
+  useEffect(() => {
+    getState()
+      .then((state) => {
+        setCurrentDoc(state);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
 
-    // Effects
-    useEffect(() => {
-        getState().then(
-            (state) => {
-                setCurrentDoc(state);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
+    listEntries()
+      .then((data) => {
+        setDocuments(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
 
-        listEntries().then(
-            (data) => {
-                setDocuments(data);
-            }
-        ).catch(
-            (error) => {
-                console.error('Error:', error);
-            }
-        )
-
-    }, []);
-
-    useEffect(() => {
-        const unlisten = listen('update_document', () => {
-            getState().then(
-                (state) => {
-                    setCurrentDoc(state);
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                });
-
-            listEntries().then(
-                (data) => {
-                    setDocuments(data);
-                }
-            ).catch(
-                (error) => {
-                    console.error('Error:', error);
-                }
-            )
+  useEffect(() => {
+    const unlisten = listen("update_document", () => {
+      getState()
+        .then((state) => {
+          setCurrentDoc(state);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
         });
 
-        // Clean up the event listener on component unmount
-        return () => {
-            unlisten.then((fn) => fn());
-        };
-    }, []);
+      listEntries()
+        .then((data) => {
+          setDocuments(data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
 
-    // Handlers
-    const handleSaveEntry = () => {
-        saveEntry()
-            .then(() => {
-                openNotification('Success', NotificationType.SUCCESS, 'Entry saved successfully.');
-            })
-            .catch((error) => {
-                openNotification('Error', NotificationType.ERROR, error.message);
-            });
-    }
+    // Clean up the event listener on component unmount
+    return () => {
+      unlisten.then((fn) => fn());
+    };
+  }, []);
 
-    const handleNewEntry = () => {
-        newEntry().catch(
-            (error) => {
-                openNotification('Error', NotificationType.ERROR, error.message);
+  // Handlers
+  const handleSaveEntry = () => {
+    saveEntry()
+      .then(() => {
+        openNotification(
+          "Success",
+          NotificationType.SUCCESS,
+          "Entry saved successfully."
+        );
+      })
+      .catch((error) => {
+        openNotification("Error", NotificationType.ERROR, error.message);
+      });
+  };
+
+  const handleNewEntry = () => {
+    newEntry().catch((error) => {
+      openNotification("Error", NotificationType.ERROR, error.message);
+    });
+  };
+
+  const handleDownload = () => {
+    exportToJSON().catch((error) => {
+      openNotification("Error", NotificationType.ERROR, error.message);
+    });
+  };
+
+  const handleLoadEntry = () => {
+    loadJSON()
+      .then(() => {
+        openNotification(
+          "Success",
+          NotificationType.SUCCESS,
+          "Entry has been loaded."
+        );
+      })
+      .catch((error) => {
+        openNotification("Error", NotificationType.ERROR, error.message);
+      });
+  };
+
+  const handleDBChange = (value: number) => {
+    loadEntry(value)
+      .then(() => {
+        emit("update_document").then(() => null);
+        openNotification(
+          "Success",
+          NotificationType.SUCCESS,
+          "Entry has been loaded."
+        );
+      })
+      .catch((error) => {
+        openNotification("Error", NotificationType.ERROR, error.message);
+      });
+  };
+
+  // List all small molecules
+  const smallMoleculeDbs = getAllSmallMolecules().then((data) => {
+    console.log(data);
+  });
+
+  return (
+    <NotificationProvider>
+      <div
+        className={"h-screen"}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+        }}
+      >
+        <Input
+          className="text-2xl font-bold"
+          value={currentDoc?.title || ""}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
+        />
+        <div className={"flex flex-row gap-2"}>
+          <Button onClick={handleNewEntry}>New Entry</Button>
+          <Button onClick={handleSaveEntry}>Save Entry</Button>
+          <Button onClick={handleLoadEntry}>Load Entry</Button>
+          <Button onClick={handleDownload}>Download</Button>
+          <Select
+            placeholder={"Select a document"}
+            onChange={handleDBChange}
+            options={documents?.map(([title, id]) => ({
+              label: title,
+              value: id,
+            }))}
+          />
+        </div>
+        <div className={"h-screen mt-2"}>
+          <div
+            className={
+              "h-full flex justify-center overflow-auto rounded-2xl scrollbar-hide"
             }
-        )
-    }
-
-    const handleDownload = () => {
-        exportToJSON().catch(
-            (error) => {
-                openNotification('Error', NotificationType.ERROR, error.message);
-            }
-        )
-    }
-
-    const handleLoadEntry = () => {
-        loadJSON()
-            .then(() => {
-                openNotification('Success', NotificationType.SUCCESS, 'Entry has been loaded.');
-            })
-            .catch(
-                (error) => {
-                    openNotification('Error', NotificationType.ERROR, error.message);
-                }
-            )
-    }
-
-    const handleDBChange = (value: number) => {
-        loadEntry(value).then(
-            () => {
-                emit('update_document').then(() => null);
-                openNotification('Success', NotificationType.SUCCESS, 'Entry has been loaded.');
-            }
-        ).catch(
-            (error) => {
-                openNotification('Error', NotificationType.ERROR, error.message);
-            }
-        )
-    }
-
-    return (
-        <NotificationProvider>
-            <div className={"h-screen"}
-                 style={
-                     {
-                         display: 'flex',
-                         flexDirection: 'column',
-                         gap: '10px'
-                     }
-                 }>
-                <Input
-                    className="text-2xl font-bold"
-                    value={currentDoc?.title || ""}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
-                />
-                <div className={"flex flex-row gap-2"}>
-                    <Button onClick={handleNewEntry}>New Entry</Button>
-                    <Button onClick={handleSaveEntry}>Save Entry</Button>
-                    <Button onClick={handleLoadEntry}>Load Entry</Button>
-                    <Button onClick={handleDownload}>Download</Button>
-                    <Select placeholder={"Select a document"}
-                            onChange={handleDBChange}
-                            options={
-                                documents?.map(
-                                    ([title, id]) => (
-                                        {
-                                            label: title,
-                                            value: id
-                                        }
-                                    )
-                                )
-                            }/>
-                </div>
-                <div className={"h-screen mt-2"}>
-                    <div className={"h-full flex justify-center overflow-auto rounded-2xl scrollbar-hide"}>
-                        <div className={"h-full w-[80%]"}
-                             style={{
-                                 borderRadius: 10,
-                                 border: '1px solid #e5e5e5'
-
-                             }}
-                        >
-                            <JsonView data={currentDoc?.doc}
-                                      shouldExpandNode={allExpanded}
-                                      style={defaultStyles}/>
-                        </div>
-                    </div>
-                </div>
+          >
+            <div
+              className={"h-full w-[80%]"}
+              style={{
+                borderRadius: 10,
+                border: "1px solid #e5e5e5",
+              }}
+            >
+              <JsonView
+                data={currentDoc?.doc}
+                shouldExpandNode={(level, value, field) => {
+                  return true;
+                }}
+                style={defaultStyles}
+              />
             </div>
-        </NotificationProvider>
-    );
+          </div>
+        </div>
+      </div>
+    </NotificationProvider>
+  );
 }
