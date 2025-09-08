221f240228e6c3bed6db986ad6b1c784333947ed
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-09-08 10:16:40 +0200

    Update LineChart to use linear scale and formatting
    Changed the xScale type from 'point' to 'linear' and added xFormat for improved axis formatting. Also refactored code for consistent style and formatting, and removed unused console log.

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,206 +1,203 @@
-import {VisData} from "../commands/visualisation.ts";
-import {LineProps, ResponsiveLine} from "@nivo/line";
-import {useEffect, useState} from "react";
+import { VisData } from "../commands/visualisation.ts";
+import { LineProps, ResponsiveLine } from "@nivo/line";
+import { useEffect, useState } from "react";
 import useAppStore from "../stores/appstore.ts";
-import {theme} from "antd";
+import { theme } from "antd";
 import useVizStore from "../stores/vizstore.ts";
 
 interface LineChartProps {
-    data: VisData;
+  data: VisData;
 }
 
 interface WindowSize {
-    width: number;
-    height: number;
+  width: number;
+  height: number;
 }
 
-export default function LineChart(
-    {data}: LineChartProps
-) {
-    // States
-    const [parentSize, setParentSize] = useState<WindowSize>({width: 0, height: 0});
-    const [chartTheme, setChartTheme] = useState<LineProps["theme"]>({});
-    const useLines = useVizStore(state => state.useLines);
-    const usePoints = useVizStore(state => state.usePoints);
-    const darkMode = useAppStore(state => state.darkMode);
+export default function LineChart({ data }: LineChartProps) {
+  // States
+  const [parentSize, setParentSize] = useState<WindowSize>({
+    width: 0,
+    height: 0,
+  });
+  const [chartTheme, setChartTheme] = useState<LineProps["theme"]>({});
+  const useLines = useVizStore((state) => state.useLines);
+  const usePoints = useVizStore((state) => state.usePoints);
+  const darkMode = useAppStore((state) => state.darkMode);
 
-    // Styles
-    const {token} = theme.useToken();
+  // Styles
+  const { token } = theme.useToken();
 
-    // Effects
-    useEffect(() => {
-        setParentSize(
-            {
-                width: window.innerWidth,
-                height: window.innerHeight,
+  // Effects
+  useEffect(() => {
+    setParentSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
 
-            }
-        )
-    }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      setParentSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
 
-    useEffect(() => {
-        const handleResize = () => {
-            setParentSize({
-                width: window.innerWidth,
-                height: window.innerHeight
-            });
-        };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-
-    useEffect(
-        () => {
-            console.log("Something: ", darkMode)
-
-            if (true === true) {
-                const newChartTheme: LineProps["theme"] = {
-                    crosshair: {
-                        line: {
-                            stroke: token.colorText,
-                            strokeWidth: 1,
-                            strokeDasharray: "2 2"
-                        }
-                    },
-                    legends: {
-                        text: {
-                            fill: token.colorTextLabel,
-                            fontSize: 12,
-                            whiteSpace: 'pre-wrap',
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                        },
-                    },
-                    tooltip: {
-                        container: {
-                            background: token.colorBgContainer,
-                            color: token.colorText,
-                        }
-                    },
-                    grid: {
-                        line: {
-                            stroke: token.colorBorder,
-                            strokeWidth: 1,
-                            strokeDasharray: "2 2"
-                        }
-                    },
-                    axis: {
-                        legend: {
-                            text: {
-                                fontSize: 18,
-                                fill: token.colorText,
-                            }
-                        },
-                        ticks: {
-                            text: {
-                                fontSize: 14,
-                                fill: token.colorText,
-                            },
-                            line: {
-                                stroke: token.colorBorder,
-                                strokeWidth: 1,
-                            }
-                        },
-                        domain: {
-                            line: {
-                                stroke: token.colorBorder,
-                                strokeWidth: 1,
-                            }
-                        }
-                    }
-                }
-
-                setChartTheme(newChartTheme)
-
-            }
-
+  useEffect(() => {
+    if (true === true) {
+      const newChartTheme: LineProps["theme"] = {
+        crosshair: {
+          line: {
+            stroke: token.colorText,
+            strokeWidth: 1,
+            strokeDasharray: "2 2",
+          },
         },
-        [darkMode]
-    )
+        legends: {
+          text: {
+            fill: token.colorTextLabel,
+            fontSize: 12,
+            whiteSpace: "pre-wrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          },
+        },
+        tooltip: {
+          container: {
+            background: token.colorBgContainer,
+            color: token.colorText,
+          },
+        },
+        grid: {
+          line: {
+            stroke: token.colorBorder,
+            strokeWidth: 1,
+            strokeDasharray: "2 2",
+          },
+        },
+        axis: {
+          legend: {
+            text: {
+              fontSize: 18,
+              fill: token.colorText,
+            },
+          },
+          ticks: {
+            text: {
+              fontSize: 14,
+              fill: token.colorText,
+            },
+            line: {
+              stroke: token.colorBorder,
+              strokeWidth: 1,
+            },
+          },
+          domain: {
+            line: {
+              stroke: token.colorBorder,
+              strokeWidth: 1,
+            },
+          },
+        },
+      };
 
-    return (
-        <div style={{
-            width: parentSize.width * 0.8,
-            height: parentSize.height * 0.8,
-        }}>
-            <ResponsiveLine
-                // @ts-ignore
-                data={data}
-                theme={chartTheme}
-                lineWidth={useLines ? 1.5 : 0}
-                animate={false}
-                margin={{top: 30, right: 170, bottom: 110, left: 80}}
-                xScale={{type: 'point'}}
-                yScale={{
-                    type: 'linear',
-                    min: 'auto',
-                    max: 'auto',
-                    reverse: false
-                }}
-                yFormat=" >-.2f"
-                curve="cardinal"
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: 'Time',
-                    legendOffset: 50,
-                    legendPosition: 'middle',
-                    truncateTickAt: 0
-                }}
-                axisLeft={{
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: "Concentration",
-                    legendOffset: -50,
-                    legendPosition: 'middle',
-                    truncateTickAt: 0
-                }}
-                enableGridX={true}
-                colors={{scheme: 'nivo'}}
-                enablePoints={true}
-                pointSize={usePoints ? 4 : 0}
-                pointColor={{from: 'color', modifiers: []}}
-                pointBorderWidth={2}
-                pointBorderColor={{from: 'color', modifiers: []}}
-                pointLabel="data.yFormatted"
-                pointLabelYOffset={-12}
-                areaBaselineValue={50}
-                enableSlices="x"
-                enableTouchCrosshair={true}
-                useMesh={true}
-                legends={[
-                    {
-                        anchor: 'top-right',
-                        direction: 'column',
-                        justify: false,
-                        translateX: 100,
-                        translateY: 0,
-                        itemsSpacing: 0,
-                        itemDirection: 'left-to-right',
-                        itemWidth: 80,
-                        itemHeight: 20,
-                        itemOpacity: 0.75,
-                        symbolSize: 12,
-                        symbolShape: 'circle',
-                        symbolBorderColor: token.colorBorder,
-                        effects: [
-                            {
-                                on: 'hover',
-                                style: {
-                                    itemBackground: 'rgba(0, 0, 0, .03)',
-                                    itemOpacity: 1
-                                }
-                            }
-                        ]
-                    }
-                ]}
-            />
-        </div>
-    )
+      setChartTheme(newChartTheme);
+    }
+  }, [darkMode]);
+
+  return (
+    <div
+      style={{
+        width: parentSize.width * 0.8,
+        height: parentSize.height * 0.8,
+      }}
+    >
+      <ResponsiveLine
+        // @ts-ignore
+        data={data}
+        theme={chartTheme}
+        lineWidth={useLines ? 1.5 : 0}
+        animate={false}
+        margin={{ top: 30, right: 170, bottom: 110, left: 80 }}
+        xScale={{
+          type: "linear",
+          min: "auto",
+          max: "auto",
+        }}
+        yScale={{
+          type: "linear",
+          min: "auto",
+          max: "auto",
+          reverse: false,
+        }}
+        xFormat=" >-.2f"
+        yFormat=" >-.2f"
+        curve="linear"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Time",
+          legendOffset: 50,
+          legendPosition: "middle",
+          truncateTickAt: 0,
+        }}
+        axisLeft={{
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Concentration",
+          legendOffset: -50,
+          legendPosition: "middle",
+          truncateTickAt: 0,
+        }}
+        enableGridX={true}
+        colors={{ scheme: "nivo" }}
+        enablePoints={true}
+        pointSize={usePoints ? 4 : 0}
+        pointColor={{ from: "color", modifiers: [] }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: "color", modifiers: [] }}
+        pointLabel="data.yFormatted"
+        pointLabelYOffset={-12}
+        areaBaselineValue={50}
+        enableSlices="x"
+        enableTouchCrosshair={true}
+        useMesh={true}
+        legends={[
+          {
+            anchor: "top-right",
+            direction: "column",
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: "left-to-right",
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: "circle",
+            symbolBorderColor: token.colorBorder,
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemBackground: "rgba(0, 0, 0, .03)",
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </div>
+  );
 }
